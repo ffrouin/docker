dc3694ded8bb4797e00bf3b8aad9706715c3c03f
ffrouin <freddy@linuxtribe.fr>
2016-08-15 11:01:56 +0200

    build script

---
+++ build_ffrouin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ubuntu 14.04 phusion/baseimage english branch
+cd baseimage-0.9.19-14.04 && make build NAME=baseimage-14.04 && cd ../
+docker build -t ffrouin:system-14.04 system-14.04
+docker build -t ffrouin:lamp-14.04 lamp-14.04
+docker build -t ffrouin:wordpress-14.04 wordpress-14.04
+
