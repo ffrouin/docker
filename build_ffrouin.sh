962f87505834e3adb4297fa8dab1afe710372e23
ffrouin <freddy@linuxtribe.fr>
2016-08-24 19:54:08 +0200

    Update

--- build_ffrouin.sh
+++ build_ffrouin.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Ubuntu 14.04 phusion/baseimage - English
-cd baseimage-0.9.19-14.04 && make build NAME=baseimage-14.04 && cd ../
+#cd baseimage-0.9.19-14.04 && make build NAME=baseimage-14.04 && cd ../
 docker build -t ffrouin:system-14.04 system-14.04
 docker build -t ffrouin:lamp-14.04 lamp-14.04
 docker build -t ffrouin:wordpress-14.04 wordpress-14.04
 docker build -t ffrouin:desktop-14.04 desktop-14.04
-docker build -t ffrouin:reprap-desktop-14.04 reprap-desktop-14.04
 
 # Ubuntu 14.04 phusion/baseimage - French
 docker build -t ffrouin:desktop-14.04-fr desktop-14.04-fr
@@ -18,9 +17,9 @@ docker build -t ffrouin:system-16.04 system-16.04
 docker build -t ffrouin:lamp-16.04 lamp-16.04
 docker build -t ffrouin:wordpress-16.04 wordpress-16.04
 docker build -t ffrouin:desktop-16.04 desktop-16.04
-docker build -t ffrouin:reprap-desktop-16.04 reprap-desktop-16.04
 
 # Ubuntu 16.04 phusion/baseimage - French
 docker build -t ffrouin:desktop-16.04-fr desktop-16.04-fr
 docker build -t ffrouin:laurux-db-16.04 laurux-db-16.04
 docker build -t ffrouin:laurux-desktop-16.04 laurux-desktop-16.04
+
