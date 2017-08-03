3ae3a5e7d5840ee3304aea23f766be774087c589
ffrouin <freddy@linuxtribe.fr>
2017-08-03 17:20:25 +0200

    suspicious

--- build_ffrouin.sh
+++ build_ffrouin.sh
@@ -9,6 +9,7 @@ docker build -t ffrouin:wordpress-14.04 wordpress-14.04
 docker build -t ffrouin:desktop-14.04 desktop-14.04
 docker build -t ffrouin:opensi-server-14.04 opensi-server-14.04
 docker build -t ffrouin:opensi-desktop-14.04 opensi-desktop-14.04
+docker build -t ffrouin:suspicious-14.04 suspicious-14.04
 
 # Ubuntu 14.04 phusion/baseimage - French
 docker build -t ffrouin:desktop-14.04-fr desktop-14.04-fr
@@ -23,6 +24,7 @@ docker build -t ffrouin:wordpress-16.04 wordpress-16.04
 docker build -t ffrouin:desktop-16.04 desktop-16.04
 docker build -t ffrouin:opensi-server-16.04 opensi-server-16.04
 docker build -t ffrouin:opensi-desktop-16.04 opensi-desktop-16.04
+docker build -t ffrouin:suspicious-16.04 suspicious-16.04
 
 # Ubuntu 16.04 phusion/baseimage - French
 docker build -t ffrouin:desktop-16.04-fr desktop-16.04-fr
